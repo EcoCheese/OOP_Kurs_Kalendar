e0fb0c0509b89495d56e8de142bfa77324633c54
Ilya Kangin <illlyakangin@gmail.com>
2019-10-16 08:56:19 +0300

    Creation of calendarView and base app design

---
+++ OOP_Kurs/DateCollectionViewCell.swift
@@ -0,0 +1,15 @@
+//
+//  DateCollectionViewCell.swift
+//  OOP_Kurs
+//
+//  Created by Ilya Kangin on 10/13/19.
+//  Copyright © 2019 Ilya Kangin. All rights reserved.
+//
+
+import UIKit
+
+class DateCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var DateLabel: UILabel!
+    
+}
--- OOP_Kurs/ViewController.swift
+++ OOP_Kurs/ViewController.swift
@@ -8,13 +8,48 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource {
+    
+    
 
+    @IBOutlet weak var Calendar: UICollectionView!
+    @IBOutlet weak var monthLabel: UILabel!
+    @IBOutlet weak var yearLabel: UILabel!
+    
+    let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
+    let daysOfWeek = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
+    let monthsLastDay = [31,28,31,30,31,30,31,31,30,31,30,31]
+    var currentMonth = String()
+ 
+    
+    
+    
+    @IBAction func Next(_ sender: Any) {
+    }
+    @IBAction func Back(_ sender: Any) {
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        currentMonth = months[month]
+        monthLabel.text = "\(currentMonth)"
+        yearLabel.text = "\(year)"
+        yearLabel.textColor = .systemGreen
     }
 
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return monthsLastDay[month]
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "Calendar", for: indexPath) as! DateCollectionViewCell
+        cell.backgroundColor = UIColor.clear
+        cell.DateLabel.text = "\(indexPath.row + 1)"
+        
+        return cell
+    }
 
 }
 
