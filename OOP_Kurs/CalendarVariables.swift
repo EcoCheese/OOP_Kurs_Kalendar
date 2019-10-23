8ffc8be5562e4f15f50561ca16e5f7c55128d8f3
Ilya Kangin <illlyakangin@gmail.com>
2019-10-24 02:12:04 +0300

    Fully working calendar with button controls. Changes of months, years with addition for february that is changed every 4th year from 28 to 29 days.

--- OOP_Kurs/CalendarVariables.swift
+++ OOP_Kurs/CalendarVariables.swift
@@ -7,6 +7,6 @@ let date = Date()
 let calendar = Calendar.current
 
 let day = calendar.component(.day, from: date)
-let weekday = calendar.component(.weekday, from: date)
-let month = calendar.component(.month, from: date) - 1
-let year = calendar.component(.year, from: date)
+var weekday = calendar.component(.weekday, from: date) - 1
+var month = calendar.component(.month, from: date) - 1
+var year = calendar.component(.year, from: date)
--- OOP_Kurs/ViewController.swift
+++ OOP_Kurs/ViewController.swift
@@ -18,17 +18,16 @@ class ViewController: UIViewController, UICollectionViewDelegate, UICollectionVi
     
     let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
     let daysOfWeek = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
-    let monthsLastDay = [31,28,31,30,31,30,31,31,30,31,30,31]
+    var monthsLastDay = [31,28,31,30,31,30,31,31,30,31,30,31]
     var currentMonth = String()
- 
     
-    
-    
-    @IBAction func Next(_ sender: Any) {
-    }
-    @IBAction func Back(_ sender: Any) {
-    }
-    
+    var numOfEmptyBox = Int() //amount of
+    var nextNumOfEmptyBox = Int()
+    var prevNumOfEmptyBox = 0
+    var direction = 0
+    var positionIndex = 0
+    var leapYear = 2
+    var dayCounter = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -37,16 +36,200 @@ class ViewController: UIViewController, UICollectionViewDelegate, UICollectionVi
         monthLabel.text = "\(currentMonth)"
         yearLabel.text = "\(year)"
         yearLabel.textColor = .systemGreen
+        
+        if weekday == 0 {
+            weekday = 7
+        }
+        
+        getStartDatePosition()
     }
+ 
+    
+    func getStartDatePosition() {
+        switch direction {
+        case 0:
+            numOfEmptyBox = weekday
+            dayCounter = day
+            while dayCounter > 0 {
+                numOfEmptyBox = numOfEmptyBox - 1
+                dayCounter = dayCounter - 1
+
+                if numOfEmptyBox == 0 {
+                     numOfEmptyBox = 7
+                }
+            }
+            if numOfEmptyBox == 7 {
+                numOfEmptyBox = 0
+            }
+            positionIndex = numOfEmptyBox
+
+             
+        case 1...:
+            nextNumOfEmptyBox = (positionIndex + monthsLastDay[month])%7
+            positionIndex = numOfEmptyBox
+        case -1:
+            prevNumOfEmptyBox = (7 - (monthsLastDay[month] - positionIndex)%7)
+            if prevNumOfEmptyBox == 7 {
+                prevNumOfEmptyBox = 0
+            }
+            positionIndex = prevNumOfEmptyBox
+        default:
+            fatalError()
+        }
+    }
+    
+    
+    
+    @IBAction func Next(_ sender: Any) {
+        switch currentMonth {
+        case "December":
+            direction = 1
+            
+            month = 0
+            year += 1
+            
+            
+            if leapYear < 5 {
+                leapYear += 1
+            }
+            
+            if leapYear == 4 {
+                monthsLastDay[1] = 29
+            }
+            
+            if leapYear == 5 {
+                leapYear = 1
+                monthsLastDay[1] = 28
+            }
+            
+            
+            
+            getStartDatePosition()
+            
+            currentMonth = months[month]
+            
+            monthLabel.text = "\(currentMonth)"
+            yearLabel.text = "\(year)"
+            
+            Calendar.reloadData()
+            
+        default:
+            
+            direction = 1
+            
+            getStartDatePosition()
+            
+            month += 1
+            
+            currentMonth = months[month]
+            monthLabel.text = "\(currentMonth)"
+            yearLabel.text = "\(year)"
+            
+            Calendar.reloadData()
+        }
+    }
+    
+    
+    @IBAction func Back(_ sender: Any) {
+        switch currentMonth {
+        case "January":
+            direction = -1
+            
+            month = 11
+            year -= 1
+            
+            if leapYear > 0 {
+                leapYear -= 1
+            }
+            
+            if leapYear == 0 {
+                monthsLastDay[1] = 29
+                leapYear = 4
+            } else {
+                monthsLastDay[1] = 28
+            }
+            
+            getStartDatePosition()
+            
+            currentMonth = months[month]
+            
+            monthLabel.text = "\(currentMonth)"
+            yearLabel.text = "\(year)"
+            
+            Calendar.reloadData()
+            
+        default:
+            direction = -1
+            
+            month -= 1
+            
+            getStartDatePosition()
+            
+            currentMonth = months[month]
+            
+            monthLabel.text = "\(currentMonth)"
+            yearLabel.text = "\(year)"
+            
+            Calendar.reloadData()
+        }
+    }
+    
 
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return monthsLastDay[month]
+        
+        switch direction {
+        case 0:
+            return monthsLastDay[month] + numOfEmptyBox
+        case 1...:
+            return monthsLastDay[month] + nextNumOfEmptyBox
+        case -1:
+            return monthsLastDay[month] + prevNumOfEmptyBox
+        default:
+            fatalError()
+        }
+        
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "Calendar", for: indexPath) as! DateCollectionViewCell
+        
         cell.backgroundColor = UIColor.clear
-        cell.DateLabel.text = "\(indexPath.row + 1)"
+        cell.DateLabel.textColor = UIColor.black
+        cell.DateLabel.font = UIFont.systemFont(ofSize: 13)
+        
+        if cell.isHidden {
+            cell.isHidden = false
+        }
+        
+        switch direction {
+        case 0:
+            cell.DateLabel.text = "\(indexPath.row + 1 - numOfEmptyBox)"
+        case 1:
+            cell.DateLabel.text = "\(indexPath.row + 1 - nextNumOfEmptyBox)"
+        case -1:
+            cell.DateLabel.text = "\(indexPath.row + 1 - prevNumOfEmptyBox)"
+        default:
+            fatalError()
+        }
+        
+        if Int(cell.DateLabel.text!)! < 1 {
+            cell.isHidden = true
+        }
+        
+        switch indexPath.row {
+        case 5,6,12,13,19,20,26,27,33,34:
+            if Int(cell.DateLabel.text!)! > 0{
+                cell.DateLabel.textColor = UIColor.systemRed
+            }
+        default:
+            break
+        }
+        
+        if currentMonth == months[calendar.component(.month, from: date) - 1] && year == calendar.component(.year, from: date) && indexPath.row + 1 - numOfEmptyBox == day{
+            cell.backgroundColor = UIColor.systemGreen
+            cell.DateLabel.textColor = UIColor.white
+            cell.DateLabel.font = UIFont.boldSystemFont(ofSize: 16)
+        }
         
         return cell
     }
